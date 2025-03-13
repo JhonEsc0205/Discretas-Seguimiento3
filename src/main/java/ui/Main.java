64451f5f8204a10082fdba57856583242add746c
Dabg5 <1110041463@u.icesi.edu.co>
2025-03-13 14:31:33 -0500

    Feature: Se creo el repositorio

---
+++ src/main/java/ui/Main.java
@@ -0,0 +1,26 @@
+package ui;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void se3_1(){
+        int[] L = {1, 2, 2, 3, 4, 4, 4, 5, 6, 6};
+        int[] R;
+        for (int i = 0; i < L.length; i++) {
+
+        }
+    }
+
+    public static void se3_2(){
+
+    }
+
+    public static void se3_3(){
+
+    }
+
+}
